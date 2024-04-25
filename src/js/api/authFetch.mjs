8cb0fb93bf198f311ef492f64b61af3d9c34d4df
aBadMan <114421846+aBadMan91@users.noreply.github.com>
2024-04-25 15:53:55 +0200

    Update authFetch.mjs

--- src/js/api/authFetch.mjs
+++ src/js/api/authFetch.mjs
@@ -3,17 +3,10 @@ import { load } from "../storage/index.mjs";
 export function headers() {
   const token = load("token");
 
-  let apiKey;
-  try {
-    apiKey = import.meta.env.VITE_NOROFF_API_KEY;
-  } catch (e) {
-    apiKey = process.env.VITE_NOROFF_API_KEY;
-  }
-
   return {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
-    "X-Noroff-API-Key": apiKey,
+    "X-Noroff-API-Key": import.meta.env.VITE_NOROFF_API_KEY,
   };
 }
 
