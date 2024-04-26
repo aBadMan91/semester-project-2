97e5c428957df02ada1c691b635a38296a44cd60
aBadMan <114421846+aBadMan91@users.noreply.github.com>
2024-04-26 15:04:34 +0200

    Fixes for vite
    - Fixes for vite
    
    - All html files in src folder

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,25 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [],
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+        listing: path.resolve(__dirname, "src/html/listing/index.html"),
+        listings: path.resolve(__dirname, "src/html/listings/index.html"),
+        profile: path.resolve(__dirname, "src/html/profile/index.html"),
+      },
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
 });
